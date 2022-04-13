7b7b2bc129f98a89df52d1bbbc5086638479871c
GauthierDrt <gauthierdaurat1@gmail.com>
2022-04-13 15:50:52 +0200

    Blabla

---
+++ tp/src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import config from 'config';
+import connect from './helpers/db_mongo/connect'
+import {logger} from './helpers/logger';
+
+const app = express();
+
+app.listen(3000);
+
+logger.info("Hallo Welt !");
